87094919f86bbc4d18516b4a12b94237be68fe39
daniel-keogh <37158241+daniel-keogh@users.noreply.github.com>
2021-06-27 18:36:05 +0100

    Create Procfile

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,6 +4,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TypeOrmConfig implements TypeOrmOptionsFactory {
   private options: TypeOrmModuleOptions = {
+    ssl: this.isProduction,
+    extra: {
+      ssl: this.isProduction ? { rejectUnauthorized: false } : null,
+    },
     type: 'postgres',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
@@ -13,6 +17,10 @@ export class TypeOrmConfig implements TypeOrmOptionsFactory {
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
   };
+
+  private get isProduction() {
+    return process.env.NODE_ENV === 'production';
+  }
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return this.options;
